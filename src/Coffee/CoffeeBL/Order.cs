e9e36cee5ec89b857ea4f867cea9e2af8417914a
Ciprian Barbu <barbu.ciprian.dragos@gmail.com>
2019-11-03 20:55:22 +0200

    Am adaugat clasa order

---
+++ src/Coffee/CoffeeBL/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeBL
+{
+    class Order
+    {
+        public Buyer customer { get; set; }
+        public Ordered_Coffee coffees { get; set; } /*voiam * cu ideea de a fi un pointer pe care se poate aloca dinamic vectorul de cafele*/
+    }
+}
