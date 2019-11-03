6b0e02b59bde7fa8b2238d2facb333ec96d8226c
Ciprian Barbu <barbu.ciprian.dragos@gmail.com>
2019-11-03 20:50:30 +0200

    Am adaugat clasa Ordered_Coffee

---
+++ src/Coffee/CoffeeBL/Ordered_Coffee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeBL
+{
+    enum coffee_type
+    {
+        mill = 0,
+        brewed = 1
+    }
+    class Ordered_Coffee
+    {
+        public Coffee coffee_order { get; set; }
+        public Syroup syroup_order { get; set; }
+        public coffee_type type { get; set; }
+
+        public int quantity { get; set; }
+    }
+}
