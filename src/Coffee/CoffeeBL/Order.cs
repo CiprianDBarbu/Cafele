5b3f499a8ca96e6465c32fce8cc88569d0b8d85d
Ciprian Barbu <barbu.ciprian.dragos@gmail.com>
2019-11-24 12:56:53 +0200

    Added order_state for Order

--- src/Coffee/CoffeeBL/Order.cs
+++ src/Coffee/CoffeeBL/Order.cs
@@ -4,15 +4,26 @@ using System.Text;
 
 namespace CoffeeBL
 {
+    public enum order_state
+    {
+        adding = 0,
+        user_finished = 1,
+        in_preparation = 2,
+        finished = 3,
+        delivered =4
+    }
+
     public class Order
     {
         public Order()
         {
             coffees = new List<Ordered_Coffee>();
             customer = new Buyer();
+            this.state = order_state.adding;
         }
         public Buyer customer { get; set; }
         public List<Ordered_Coffee> coffees { get; set; } 
+        public order_state state { get; set; }
 
         public float total_price 
         { 
